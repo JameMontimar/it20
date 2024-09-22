032b4074246dad07ad3a73c626f962008899264a
JameMontimar <jamesberthmontimar@gmail.com>
2024-09-22 23:44:57 +0800

    My Java Project

---
+++ src/it20_montimar/It20_montimar.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it20_montimar;
+
+/**
+ *
+ * @author Erick James Rubin
+ */
+public class It20_montimar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numbers = 23;  // value of variable 'numbers' is 23
+        
+        // Check if 'numbers' is greater than 6
+        if (numbers > 6){
+            // If the condition is true, it prints "Lebron is the goat" followed by the value of 'numbers'
+            System.out.print("Lebron is the goat" + " #" + numbers);  
+        } else {
+            // If the condition is false, it prints "Lebron is not the goat" with an alternative message
+            System.out.print("Lebron is not the goat" + " real goat is Jorbum");
+        }
+    }
+    
+}
